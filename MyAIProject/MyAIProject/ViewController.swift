f1548bebfcb6cb5b2b89bae868f11d803e2ffc22
杨旭根 <652164@ky-tech.com.cn>
2026-05-20 21:05:34 +0800

    init

---
+++ MyAIProject/MyAIProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAIProject
+//
+//  Created by kye-yxg on 2026/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
