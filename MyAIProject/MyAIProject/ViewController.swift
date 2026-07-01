509c8bc898f5dba5fd375d708f0f90f3375de5cf
杨旭根 <652164@ky-tech.com.cn>
2026-07-01 20:34:54 +0800

    增加label

--- MyAIProject/MyAIProject/ViewController.swift
+++ MyAIProject/MyAIProject/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let label = UILabel(frame: CGRect(x: 10, y: 10, width: 100, height: 35))
+        self.view.addSubview(label)
         // Do any additional setup after loading the view.
     }
 
