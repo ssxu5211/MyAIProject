5eef092a9059ef45229ff600f83aeb9c568fa098
杨旭根 <652164@ky-tech.com.cn>
2026-07-02 23:49:53 +0800

    回退到509c8bc之前

--- MyAIProject/MyAIProject/ViewController.swift
+++ MyAIProject/MyAIProject/ViewController.swift
@@ -11,11 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        let label = UILabel(frame: CGRect(x: 10, y: 10, width: 100, height: 35))
-        let view = UIView()
-        view.frame = CGRect(x: 20, y: 20, width: 200, height: 40)
-        self.view.addSubview(label)
-        // Do any additional setup after loading the view.
+                // Do any additional setup after loading the view.
     }
 
 
