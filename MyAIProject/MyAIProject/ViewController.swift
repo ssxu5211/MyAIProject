9cf6d38a4429a76e55b34fecbd4e8c884b7836d0
杨旭根 <652164@ky-tech.com.cn>
2026-07-02 23:51:50 +0800

    Revert "回退到5eef092之前，也就是撤销回退"
    This reverts commit 5eef092a9059ef45229ff600f83aeb9c568fa098.

--- MyAIProject/MyAIProject/ViewController.swift
+++ MyAIProject/MyAIProject/ViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-                // Do any additional setup after loading the view.
+        let label = UILabel(frame: CGRect(x: 10, y: 10, width: 100, height: 35))
+        let view = UIView()
+        view.frame = CGRect(x: 20, y: 20, width: 200, height: 40)
+        self.view.addSubview(label)
+        // Do any additional setup after loading the view.
     }
 
 
