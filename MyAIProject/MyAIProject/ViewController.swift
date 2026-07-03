8c98e8067e6b9edddae6ce08a05c6227c00a0c0c
杨旭根 <652164@ky-tech.com.cn>
2026-07-03 11:18:42 +0800

    添加背景色

--- MyAIProject/MyAIProject/ViewController.swift
+++ MyAIProject/MyAIProject/ViewController.swift
@@ -7,15 +7,17 @@
 
 import UIKit
 
-// 这是第三次提交
+// 这是第四次提交
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let label = UILabel(frame: CGRect(x: 10, y: 10, width: 100, height: 35))
         let view = UIView()
-        view.frame = CGRect(x: 20, y: 20, width: 200, height: 40)
+        view.backgroundColor = UIColor.red
+        view.frame = CGRect(x: 20, y: 120, width: 200, height: 40)
         //
         self.view.addSubview(label)
+        self.view.addSubview(view)
         // Do any additional setup after loading the view.
     }
 
